b75d526eb81d96aa327416cf2f5a80d29a482ade
STARBOW <starbow.dev@gmail.com>
2025-03-18 19:18:31 +0900

    ADD : 페이드 연출 추가

---
+++ Prison Escape/Assets/FadePanel.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadePanel : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    void Start()
+    {
+        image.color = new Color(
+            image.color.r,
+            image.color.g,
+            image.color.b,
+            0f
+        );
+    }
+    
+    public Coroutine FadeOut(float duration)
+    {
+        return StartCoroutine(FadeOutSequence(duration));
+    }
+    
+    public Coroutine FadeIn(float duration)
+    {
+        return StartCoroutine(FadeInSequence(duration));
+    }
+
+    IEnumerator FadeOutSequence(float duration)
+    {
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            yield return null;
+            
+            currentTime += Time.deltaTime;
+            
+            image.color = new Color(
+                image.color.r,
+                image.color.g,
+                image.color.b,
+                Mathf.Lerp(0f, 1f, currentTime / duration)
+            );
+        }
+    }
+
+    IEnumerator FadeInSequence(float duration)
+    {
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            yield return null;
+            
+            currentTime += Time.deltaTime;
+            
+            image.color = new Color(
+                image.color.r,
+                image.color.g,
+                image.color.b,
+                Mathf.Lerp(1f, 0f, currentTime / duration)
+            );
+        }
+    }
+}
--- Prison Escape/Assets/Scripts/ScatteredKey.cs
+++ Prison Escape/Assets/Scripts/ScatteredKey.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,22 +8,32 @@ public class ScatteredKey : MonoBehaviour, IItemInteractable
     // 상호작용을 위해 필요한 아이템
     [SerializeField] private GameObject needItem;
     [SerializeField] private List<GameObject> spawningItem;
+    [SerializeField] private FadePanel fadePanel;
     
     public void InteractUseItem(GameObject actor, GameObject useItem)
     {
         // 사용한 아이템과 필요한 아이템이 일치하면
         if (useItem == needItem)
         {
-            // TODO : 화면 암전
-
-            foreach (GameObject item in spawningItem)
-            {
-                item.SetActive(true);
-            }
-            
-            // TODO : 화면 암전 해제
-            
-            Destroy(useItem);
+            StartCoroutine(InteractSequence(useItem));
         }
+    }
+
+    private IEnumerator InteractSequence(GameObject useItem)
+    {
+        GetComponent<Collider>().enabled = false;
+        yield return fadePanel.FadeOut(1.0f);
+
+        foreach (GameObject item in spawningItem)
+        {
+            item.SetActive(true);
+        }
+        GetComponent<Renderer>().enabled = false;
+        Destroy(useItem);
+        yield return new WaitForSeconds(0.5f);
+            
+        yield return fadePanel.FadeIn(1.0f);
+        
+        Destroy(gameObject);
     }
 }
