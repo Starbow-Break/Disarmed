7d54ccbcd2e4d0db62b5dbd707bd121bf8e491ba
Seokhwan98 <seokhwan98@naver.com>
2025-03-21 14:28:20 +0900

    ADD: 책 글씨 추가 및 Dialogue 수정

--- Prison Escape/Assets/Scripts/UI/StartDialogue.cs
+++ Prison Escape/Assets/Scripts/UI/StartDialogue.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class StartWitch : BaseDialogue
+public class StartDialogue : BaseDialogue
 {
     private void OnTriggerEnter(Collider other)
     {
