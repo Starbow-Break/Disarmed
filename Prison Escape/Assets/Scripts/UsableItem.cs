7e8c4f5ccfdc18565b18fd584a4c36ca01469166
STARBOW <starbow.dev@gmail.com>
2025-03-24 17:57:18 +0900

    FIX : 아이템 사용 시 Destroy된 오브젝트에 접근하는 현상 수정

--- Prison Escape/Assets/Scripts/UsableItem.cs
+++ Prison Escape/Assets/Scripts/UsableItem.cs
@@ -7,10 +7,11 @@ public class UsableItem : PickableItem, IUsable
     [field: SerializeField]
     public UnityEvent OnUse { get; private set; }
 
-    public void Use(GameObject actor)
+    public void Use(GameObject actor, GameObject target)
     {
         Debug.Log($"Use {gameObject.name}!");
         OnUse?.Invoke();
         Destroy(gameObject);
+        actor.GetComponent<EventHandler>()?.SetNullUsable();
     }
 }
