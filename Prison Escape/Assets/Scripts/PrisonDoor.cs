c60aceb83101cae9113e1302411454db84f75475
STARBOW <starbow.dev@gmail.com>
2025-03-24 13:23:20 +0900

    ADD : 문이 잠겨있을 대 상호작용하면 문이 잠긴 효과음 추가

--- Prison Escape/Assets/Scripts/PrisonDoor.cs
+++ Prison Escape/Assets/Scripts/PrisonDoor.cs
@@ -4,7 +4,13 @@ public class PrisonDoor : MonoBehaviour, IItemInteractable
 {
     // 상호작용을 위해 필요한 아이템
     [SerializeField] private GameObject needItem;
+
+    [SerializeField] private AudioClip failClip; // 상호작용 실패 시 사운드
+    [SerializeField] private AudioClip successClip; // 상호작용 성공 시 사운드
+    
     private Animator animator;
+    private AudioSource audioSource;
+    
     void Awake()
     {
         animator = GetComponentInParent<Animator>();
@@ -15,7 +21,12 @@ public class PrisonDoor : MonoBehaviour, IItemInteractable
         // 사용한 아이템과 필요한 아이템이 일치하면
         if (useItem == needItem)
         {
+            audioSource.PlayOneShot(successClip);
             animator.SetTrigger("Open");
+        }
+        else
+        {
+            audioSource.PlayOneShot(failClip);
         }
     }
 }
