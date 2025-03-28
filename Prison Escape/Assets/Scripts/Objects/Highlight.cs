4d25c9901d8bef4638025ad9da3c0f2f5d22c5aa
STARBOW <starbow.dev@gmail.com>
2025-03-28 16:47:34 +0900

    FIX : 하이라이트를 아웃라인 형태로 수정

--- Prison Escape/Assets/Scripts/Objects/Highlight.cs
+++ Prison Escape/Assets/Scripts/Objects/Highlight.cs
@@ -1,41 +1,78 @@
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
+[Serializable]
+public class ShaderPropertySettings<T>
+{
+    public string property;
+    public T value;
+}
 
 public class Highlight : MonoBehaviour
 {
-    [SerializeField] private List<Renderer> renderers;      // 오브젝트에서 사용하는 렌더러들
-    [SerializeField] Color highlightColor = Color.white;    // 하이라이트 색
+    [Header("Highlight Material")]
+    [SerializeField] private Material HighlightMaterial;
+    
+    [Header("Material Settings")]
+    [SerializeField] List<ShaderPropertySettings<float>> floatProperties = new();
+    [SerializeField] List<ShaderPropertySettings<Color>> colorProperties = new();
 
-    private List<Material> materials;   // 사용하는 모든 머터리얼들
+    private new Renderer renderer; // 렌더러
+    private Material[] originalMaterials; // 원래 머티리얼들
 
     private void Awake()
     {
-        // 렌더러에 할당된 모든 머터리얼들을 추출
-        materials = new List<Material>();
-        foreach(var renderer in renderers)
+        renderer = GetComponent<Renderer>();
+        originalMaterials = renderer.materials;
+
+        if (HighlightMaterial == null)
         {
-            materials.AddRange(new List<Material>(renderer.materials));
+            Debug.LogError("Outline Materials are not assigned");
+            return;
         }
+
+        SetMaterialProperty();
     }
 
+    private void Start()
+    {
+        // 처음에는 비활성화
+        SetHighlight(false);
+    }
+    
+    // 하이라이트 머터리얼의 프로퍼티를 설정
+    private void SetMaterialProperty()
+    {
+        foreach (ShaderPropertySettings<float> floatProperty in floatProperties)
+        {
+            HighlightMaterial.SetFloat(Shader.PropertyToID(floatProperty.property), floatProperty.value);
+        }
+        foreach (ShaderPropertySettings<Color> colorProperty in colorProperties)
+        {
+            HighlightMaterial.SetColor(Shader.PropertyToID(colorProperty.property), colorProperty.value);
+        }
+    }
+    
+    // 하이라이트 여부 설정
     public void SetHighlight(bool value)
     {
+        if (HighlightMaterial == null)
+        {
+            return;
+        }
+        
         if (value)
         {
-            // emission 키워드 활성화 및 emission color를 highlight color로 변경
-            foreach (var material in materials)
-            {
-                material.EnableKeyword("_EMISSION");
-                material.SetColor("_EmissionColor", highlightColor);
-            }
+            Material[] tempMaterials = new Material[originalMaterials.Length + 1];
+            originalMaterials.CopyTo(tempMaterials, 0);
+            tempMaterials[tempMaterials.Length - 1] = HighlightMaterial;
+            renderer.materials = tempMaterials;
         }
         else
         {
-            foreach (var material in materials)
-            {
-                // emission 키워드 비활성화
-                material.DisableKeyword("_EMISSION");
-            }
+            renderer.materials = originalMaterials;
         }
     }
 }
