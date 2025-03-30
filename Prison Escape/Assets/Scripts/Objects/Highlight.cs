0e1c439bbe2e78b3d3a2f7050969b021069df524
Starbow-Break <phsyobs@naver.com>
2025-03-30 22:07:06 +0900

    FIX : Outline이 깨져서 보이는 현상 수정

--- Prison Escape/Assets/Scripts/Objects/Highlight.cs
+++ Prison Escape/Assets/Scripts/Objects/Highlight.cs
@@ -1,78 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-
-[Serializable]
-public class ShaderPropertySettings<T>
-{
-    public string property;
-    public T value;
-}
 
 public class Highlight : MonoBehaviour
 {
-    [Header("Highlight Material")]
-    [SerializeField] private Material HighlightMaterial;
-    
-    [Header("Material Settings")]
-    [SerializeField] List<ShaderPropertySettings<float>> floatProperties = new();
-    [SerializeField] List<ShaderPropertySettings<Color>> colorProperties = new();
+    [SerializeField] Outline outline;
 
-    private new Renderer renderer; // 렌더러
-    private Material[] originalMaterials; // 원래 머티리얼들
-
-    private void Awake()
-    {
-        renderer = GetComponent<Renderer>();
-        originalMaterials = renderer.materials;
-
-        if (HighlightMaterial == null)
-        {
-            Debug.LogError("Outline Materials are not assigned");
-            return;
-        }
-
-        SetMaterialProperty();
-    }
+    private bool isEnabled = false;
+    private bool dirtyFlag = false;
 
     private void Start()
     {
         // 처음에는 비활성화
         SetHighlight(false);
     }
-    
-    // 하이라이트 머터리얼의 프로퍼티를 설정
-    private void SetMaterialProperty()
+
+    private void Update()
     {
-        foreach (ShaderPropertySettings<float> floatProperty in floatProperties)
+        if (dirtyFlag)
         {
-            HighlightMaterial.SetFloat(Shader.PropertyToID(floatProperty.property), floatProperty.value);
-        }
-        foreach (ShaderPropertySettings<Color> colorProperty in colorProperties)
-        {
-            HighlightMaterial.SetColor(Shader.PropertyToID(colorProperty.property), colorProperty.value);
+            dirtyFlag = false;
+            outline.enabled = isEnabled;
         }
     }
-    
+
     // 하이라이트 여부 설정
     public void SetHighlight(bool value)
     {
-        if (HighlightMaterial == null)
-        {
-            return;
-        }
-        
-        if (value)
-        {
-            Material[] tempMaterials = new Material[originalMaterials.Length + 1];
-            originalMaterials.CopyTo(tempMaterials, 0);
-            tempMaterials[tempMaterials.Length - 1] = HighlightMaterial;
-            renderer.materials = tempMaterials;
-        }
-        else
-        {
-            renderer.materials = originalMaterials;
-        }
+        isEnabled = value;
+        dirtyFlag = true;
     }
 }
