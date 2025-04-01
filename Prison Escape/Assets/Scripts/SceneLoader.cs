834ff49cd1db01754055c996c2b65b479badea3f
Seokhwan98 <seokhwan98@naver.com>
2025-04-01 16:55:11 +0900

    ADD: Main 씬 캐릭터

--- Prison Escape/Assets/Scripts/SceneLoader.cs
+++ Prison Escape/Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private FadePanel fadePanel;
+    [SerializeField] private float fadeDuration;
+    
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        // StartCoroutine(FadeIn());
+        CameraSwitcher.instance.SwitchCamera(
+            cameraName: "Eye Camera",
+            afterSwitch: () => StartGameScene());
+            //afterSwitch: () => SceneManager.LoadScene(sceneName));
+    }
+
+    private IEnumerator FadeIn()
+    {
+        yield return fadePanel.FadeIn(fadeDuration);
+    }
+
+    // 이거는 게임 씬 안에서 시작할때 재생할거 여기서 테스트
+    private void StartGameScene()
+    {
+        
     }
 }
