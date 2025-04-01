b3d536b50a28f25645f5942f5a09f28d5dfcaa30
Seokhwan98 <seokhwan98@naver.com>
2025-04-01 17:34:41 +0900

    FIX: SceneLoader.cs 수정하면 안돼

--- Prison Escape/Assets/Scripts/SceneLoader.cs
+++ Prison Escape/Assets/Scripts/SceneLoader.cs
@@ -1,29 +1,10 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    [SerializeField] private FadePanel fadePanel;
-    [SerializeField] private float fadeDuration;
-    
     public void LoadScene(string sceneName)
     {
-        // StartCoroutine(FadeIn());
-        CameraSwitcher.instance.SwitchCamera(
-            cameraName: "Eye Camera",
-            afterSwitch: () => StartGameScene());
-            //afterSwitch: () => SceneManager.LoadScene(sceneName));
-    }
-
-    private IEnumerator FadeIn()
-    {
-        yield return fadePanel.FadeIn(fadeDuration);
-    }
-
-    // 이거는 게임 씬 안에서 시작할때 재생할거 여기서 테스트
-    private void StartGameScene()
-    {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
