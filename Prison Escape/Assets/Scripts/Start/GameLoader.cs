0529b8bd0b1f641d68215c50f823ca897f3f8149
Seokhwan98 <seokhwan98@naver.com>
2025-04-03 12:48:23 +0900

    ADD: 시작 가이드

--- Prison Escape/Assets/Scripts/Start/GameLoader.cs
+++ Prison Escape/Assets/Scripts/Start/GameLoader.cs
@@ -1,18 +1,26 @@
 using System.Collections;
+using TMPro;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLoader : MonoBehaviour
 {
+    [SerializeField] private DialogueData startDialogue;
     [SerializeField] private CinemachineCamera standup;
+    [SerializeField] private Canvas startCanvas;
+    [SerializeField] private Image controlGuide;
+    [SerializeField] private TextMeshProUGUI startMessage;
     [SerializeField] private FadePanel fadePanel;
     [SerializeField] private float fadeDuration;
     private static bool _firstTime = true;
+    private static int _startCounter = 0;
     
     private IEnumerator Start()
     {
         if (!_firstTime)
         {
+            startCanvas.enabled = false;
             standup.Priority = 0;
             PlayerLoading.LoadDelay();
         }
@@ -20,21 +28,28 @@ public class GameLoader : MonoBehaviour
         else
         {
             _firstTime = false;
+            startCanvas.enabled = true;
+            controlGuide.gameObject.SetActive(false);
+            
             PlayerLoading.PlayerSetStop();
+            startMessage.text = startDialogue.dialogues[_startCounter++];
+            
             yield return StartCoroutine(StartFadeOut());
-            yield return new WaitForSeconds(0.5f);
+            // yield return new WaitForSeconds(0.5f);
 
+            startMessage.text = "";
             CameraSwitcher.instance.SwitchCamera(
                 cameraName: "AfterStand Camera",
-                afterSwitch: () => SwitchToPlayer());
+                afterSwitch: SwitchToPlayer);
         }
     }
     
     private void SwitchToPlayer()
     {
+        startMessage.text = startDialogue.dialogues[_startCounter];
         CameraSwitcher.instance.SwitchCamera(
             cameraName: "Player Camera", 
-            afterSwitch: () => GameStart());
+            afterSwitch: GameStart);
     }
     
     private IEnumerator StartFadeOut()
@@ -44,6 +59,16 @@ public class GameLoader : MonoBehaviour
 
     private void GameStart()
     {
+        StartCoroutine(AfterStandUp());
         PlayerLoading.PlayerSetStart();
+    }
+
+    private IEnumerator AfterStandUp()
+    {
+        controlGuide.gameObject.SetActive(true);
+        startMessage.text = "";
+        yield return new WaitForSeconds(3f);
+        
+        controlGuide.gameObject.SetActive(false);
     }
 }
